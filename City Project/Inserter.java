d6db2e09c730883bc905a67bbd9a63c7ff4539d8
edgorrell <edgorrell@stu.naperville203.org>
2023-01-25 22:44:22 -0600

    need to change close hand sprite but ye

--- City Project/Inserter.java
+++ City Project/Inserter.java
@@ -6,35 +6,55 @@ import javax.imageio.*;
 import java.awt.image.*;
 
 public class Inserter extends JComponent implements Runnable{
-    BufferedImage sprite, arm, item;
+    BufferedImage base, armOpen, armClosed, item;
     int x,y,angle;
     boolean turnDirection;
-    
-    public Inserter(){}
+
     public Inserter(int x, int y){
-    
+        this.x = x;
+        this.y = y;
+        try{
+            this.base = ImageIO.read(new File("sprites/inserter/inserter-base.png"));
+            this.armOpen = ImageIO.read(new File("sprites/inserter/inserter-hand-open.png"));
+            //this.armClosed
+        } catch(Exception e){}
     }
-    public BufferedImage rotate(BufferedImage image, double theta){
-        BufferedImage result = new BufferedImage(100,100, image.getType());
-        
-        return result;
+    public BufferedImage rotate(BufferedImage img, double theta){
+        double radian = Math.toRadians(theta);
+        double sin = Math.abs(Math.sin(radian));
+        double cos = Math.abs(Math.cos(radian));
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        int nWidth = 100;
+        int nHeight = 100;
+
+        BufferedImage rotatedImage = new BufferedImage(nWidth, nHeight, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics = rotatedImage.createGraphics();
+
+        graphics.setRenderingHint(
+            RenderingHints.KEY_INTERPOLATION,
+            RenderingHints.VALUE_INTERPOLATION_BICUBIC
+        );
+
+        graphics.translate((nWidth - width) / 2, (nHeight - height) / 2);
+        graphics.rotate(radian, (double) (width / 2), (double) (height / 2));
+        graphics.drawImage(img, 0, 0, null);
+        graphics.dispose();
+
+        return rotatedImage;
     }
-    public void nextFrame(){
-        repaint();
-    }
-    @Override
-    public void paintComponent(Graphics g){
-        Graphics2D g2 = (Graphics2D) g;
-        draw(g2);
-    }
-    public void draw (Graphics2D frame){ // actually draw stuff here
-        
+    public void draw (Graphics2D frame, boolean first){ // actually draw stuff here
+        if(first){
+            frame.drawImage(this.base,this.x+35,this.y+38,null);
+        } else {
+            frame.drawImage(rotate(this.armOpen,this.angle),this.x,this.y,null);
+        }
     }
     public void run(){
-        while(true){
-            if(this.item != null && this.angle == 0){
-                this.turnDirection = Math.random() < 0.5;
-            }
-        }
+        this.angle++;
+        this.angle %= 360;
     }
 }
--- City Project/InserterManager.java
+++ City Project/InserterManager.java
@@ -6,12 +6,18 @@ import javax.imageio.*;
 import java.awt.image.*;
 
 public class InserterManager extends JComponent implements Runnable{
+    ArrayList<Inserter> inserters;
     Inserter i1, i2, i3;
+    boolean first = true;
     
     public InserterManager(){
-        this.i1 = new Inserter();
-        this.i2 = new Inserter();
-        this.i3 = new Inserter();
+        this.inserters = new ArrayList<Inserter>();
+        this.i1 = new Inserter(167,165);
+        this.i2 = new Inserter(205,165);
+        this.i3 = new Inserter(243,165);
+        inserters.add(i1);
+        inserters.add(i2);
+        inserters.add(i3);
     }
     public void nextFrame(){
         repaint();
@@ -22,9 +28,21 @@ public class InserterManager extends JComponent implements Runnable{
         draw(g2);
     }
     public void draw (Graphics2D frame){ // actually draw stuff here
-        
+        for(int i = 0; i < 2; i++){
+            for(Inserter inserter : inserters){
+                inserter.draw(frame, this.first);
+            }
+            first = !first;
+        }
     }
     public void run(){
-        
+        while(true){
+            for(Inserter i : inserters){
+                i.run();
+            }
+            try{
+                Thread.sleep(17);
+            } catch(Exception e){}
+        }
     }
 }
